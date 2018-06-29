0aaee4e5e03083af6c45fbc6ed379f62f0ca779d
Katie Weber <k.weber0893@gmail.com>
2018-06-29 13:27:33 -0400

    Built Puppy Listings

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @puppy_name = new_puppy.name
+    @puppy_breed = new_puppy.breed
+    @puppy_age = new_puppy.age
+
+    erb :display_puppy
+  end
 
 end
